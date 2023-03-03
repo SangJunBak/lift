f422c742c189cfa84dec2543a7e84efbf154eb1e
jun <sangjun.bak99@gmail.com>
2023-03-03 14:24:40 -0500

    changes paths

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {setupDb} from './src/utils/setupDb';
+import {setupDb} from 'src/utils/setupDb';
 
-import {database} from './db';
+import {database} from 'db';
 
 AppRegistry.registerComponent(appName, () => App);
 
